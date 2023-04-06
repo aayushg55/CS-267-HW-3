9a1b5027f53d0c471a36ed02aeacfbb28cf61a3a
Aayush Gupta <aayushnj@gmail.com>
2023-04-05 17:12:14 -0700

    old approach -- broken

--- hash_map.hpp
+++ hash_map.hpp
@@ -73,20 +73,22 @@ int HashMap::get_target_slot(const uint64_t &key) {
 bool HashMap::insert(const kmer_pair& kmer) {
     //get rank
     // std::cout << "in insert" << shared_used[0] << "\n";
-    // std::cout << upcxx::rank_me() << " in insert used global " << used_global << " mysize " << my_size << "\n";
+    // std::cout << upcxx::rank_me() << " in insert used global " << used_global << " mysize " << my_size << "\n";urn true;
     uint64_t hash = kmer.hash();
 
     //linear probing
     uint64_t probe = 0;
     bool success = false;
     do {
-        std::cout << "at start \n";
+        // std::cout << "at start \n";
         uint64_t slot = (hash + probe++) % global_size();
         success = request_slot(slot);
-        std::cout << probe << "\n";
+
+        // std::cout << probe << "\n";
         if (success) {
             write_slot(slot, kmer);
         }
+        // return true;
     } while (!success && probe < global_size());
     return success;
 }
@@ -94,16 +96,20 @@ bool HashMap::insert(const kmer_pair& kmer) {
 bool HashMap::find(const pkmer_t& key_kmer, kmer_pair& val_kmer) {
     uint64_t hash = key_kmer.hash();
     uint64_t probe = 0;
+    std::cout << "in find " << hash << " with " << upcxx::rank_me() << "\n";
     bool success = false;
     do {
         uint64_t slot = (hash + probe++) % global_size();
         if (slot_used(slot)) {
+            std::cout << "found potential slot " << hash << " with " << upcxx::rank_me() << "\n";
+
             val_kmer = read_slot(slot);
             if (val_kmer.kmer == key_kmer) {
                 success = true;
             }
         }
     } while (!success && probe < global_size());
+    std::cout << "finished find " << hash << "\n";
     return success;
 }
 
@@ -119,9 +125,9 @@ bool HashMap::slot_used(uint64_t slot) {
 void HashMap::write_slot(uint64_t slot, const kmer_pair& kmer) { 
     int target_rank = get_target_rank(slot);
     int target_slot = get_target_slot(slot);
-    std::cout << slot << " in write slot my rank: " << upcxx::rank_me() << " rank target: " << target_rank << "\n";
-    // rput(kmer, shared_data[target_rank] + target_slot).wait();
-    std::cout << slot << " finished write slot my rank: " << upcxx::rank_me() << " rank target: " << target_rank << "\n";
+    // std::cout << slot << " in write slot my rank: " << upcxx::rank_me() << " rank target: " << target_rank << "\n";
+    rput(kmer, shared_data[target_rank] + target_slot).wait();
+    // std::cout << slot << " finished write slot my rank: " << upcxx::rank_me() << " rank target: " << target_rank << "\n";
     // data[slot] = kmer; 
 }
 
@@ -134,15 +140,15 @@ kmer_pair HashMap::read_slot(uint64_t slot) {
 bool HashMap::request_slot(uint64_t slot) {
     int target_rank = get_target_rank(slot);
     int target_slot = get_target_slot(slot);
-    std::cout << size() << " slot: " << slot << " target rank " << target_rank << " target_slot " << target_slot << "\n";
+    // std::cout << size() << " slot: " << slot << " target rank " << target_rank << " target_slot " << target_slot << "\n";
     upcxx::atomic_domain<int> ad_int({upcxx::atomic_op::load,upcxx::atomic_op::fetch_add});
     int prev = ad_int.fetch_add(shared_used[target_rank] + target_slot, 1, std::memory_order_relaxed).wait();
     // cout << "saw a " << prev << "in "
     // upcxx::global_ptr<int> hits = upcxx::broadcast(upcxx::new_<int>(0), 0).wait();
     // shared_used[target_rank] + target_slot
     ad_int.destroy();
-    return (prev == 0);
-    // return 1;
+    // return (prev == 0);
+    return 1;
 }
 
 size_t HashMap::size() const noexcept { return my_size; }
